9cceba9e066250657e5ce9809777332a7439a9e0
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2023-03-21 20:59:07 +0800

    fix: review feedback
    remove pyteal dependecy

--- tests/test_app_client.py
+++ tests/test_app_client.py
@@ -1,6 +1,6 @@
 import pytest
 from algokit_utils.account import get_account
-from algokit_utils.application_client import ABICallArgs, ApplicationClient
+from algokit_utils.application_client import ABICallArgs, ApplicationClient, get_app_id_from_tx_id
 from algokit_utils.application_specification import ApplicationSpecification
 from algokit_utils.network_clients import get_algod_client, get_indexer_client
 from algosdk.atomic_transaction_composer import AtomicTransactionComposer
@@ -22,48 +22,59 @@ def client_fixture(app_spec: ApplicationSpecification) -> ApplicationClient:
     creator_name = get_unique_name()
     creator = get_account(algod_client, creator_name)
 
-    client = ApplicationClient(algod_client, indexer_client, app_spec, creator)
+    client = ApplicationClient(algod_client, indexer_client, app_spec, creator=creator)
     return client
 
 
 def test_bare_create(client_fixture: ApplicationClient) -> None:
-    client_fixture.create(None)
+    client_fixture.create(abi_method=False)
 
-    assert client_fixture.call("hello", args={"name": "test"}).abi_results[0].return_value == "Hello Bare, test"
+    assert client_fixture.call("hello", args={"name": "test"}).abi_result.return_value == "Hello Bare, test"
 
 
 def test_abi_create(client_fixture: ApplicationClient) -> None:
     client_fixture.create("create")
 
-    assert client_fixture.call("hello", args={"name": "test"}).abi_results[0].return_value == "Hello ABI, test"
+    assert client_fixture.call("hello", args={"name": "test"}).abi_result.return_value == "Hello ABI, test"
 
 
 def test_abi_create_args(client_fixture: ApplicationClient, app_spec: ApplicationSpecification) -> None:
     create = next(m for m in app_spec.contract.methods if m.name == "create_args")
     client_fixture.create(create, args={"greeting": "ahoy"})
 
-    assert client_fixture.call("hello", args={"name": "test"}).abi_results[0].return_value == "ahoy, test"
+    assert client_fixture.call("hello", args={"name": "test"}).abi_result.return_value == "ahoy, test"
 
 
 def test_create_auto_find(client_fixture: ApplicationClient) -> None:
     client_fixture.create(on_complete=OnComplete.OptInOC)
 
-    assert client_fixture.call("hello", args={"name": "test"}).abi_results[0].return_value == "Opt In, test"
+    assert client_fixture.call("hello", args={"name": "test"}).abi_result.return_value == "Opt In, test"
 
 
 def test_abi_create_with_atc(client_fixture: ApplicationClient) -> None:
     atc = AtomicTransactionComposer()
-    client_fixture.create("create", atc=atc)
+    client_fixture.compose_create(atc, "create")
 
-    assert not client_fixture.app_id
-    atc.execute(client_fixture.algod_client, 4)
-    assert client_fixture.app_id
+    create_result = atc.execute(client_fixture.algod_client, 4)
+    client_fixture.app_id = get_app_id_from_tx_id(client_fixture.algod_client, create_result.tx_ids[0])
+
+    assert client_fixture.call("hello", args={"name": "test"}).abi_result.return_value == "Hello ABI, test"
+
+
+def test_bare_create_with_atc(client_fixture: ApplicationClient) -> None:
+    atc = AtomicTransactionComposer()
+    client_fixture.compose_create(atc, abi_method=False)
+
+    create_result = atc.execute(client_fixture.algod_client, 4)
+    client_fixture.app_id = get_app_id_from_tx_id(client_fixture.algod_client, create_result.tx_ids[0])
+
+    assert client_fixture.call("hello", args={"name": "test"}).abi_result.return_value == "Hello Bare, test"
 
 
 def test_abi_call_with_atc(client_fixture: ApplicationClient) -> None:
     client_fixture.create("create")
     atc = AtomicTransactionComposer()
-    client_fixture.call("hello", args={"name": "test"}, atc=atc)
+    client_fixture.compose_call(atc, "hello", args={"name": "test"})
     result = atc.execute(client_fixture.algod_client, 4)
 
     assert result.abi_results[0].return_value == "Hello ABI, test"
@@ -72,9 +83,9 @@ def test_abi_call_with_atc(client_fixture: ApplicationClient) -> None:
 def test_abi_call_multiple_times_with_atc(client_fixture: ApplicationClient) -> None:
     client_fixture.create("create")
     atc = AtomicTransactionComposer()
-    client_fixture.call("hello", args={"name": "test"}, atc=atc)
-    client_fixture.call("hello", args={"name": "test2"}, atc=atc)
-    client_fixture.call("hello", args={"name": "test3"}, atc=atc)
+    client_fixture.compose_call(atc, "hello", args={"name": "test"})
+    client_fixture.compose_call(atc, "hello", args={"name": "test2"})
+    client_fixture.compose_call(atc, "hello", args={"name": "test3"})
     result = atc.execute(client_fixture.algod_client, 4)
 
     assert result.abi_results[0].return_value == "Hello ABI, test"
@@ -90,22 +101,22 @@ def test_deploy_with_create(client_fixture: ApplicationClient) -> None:
         ),
     )
 
-    assert client_fixture.call("hello", args={"name": "test"}).abi_results[0].return_value == "Hello ABI, test"
+    assert client_fixture.call("hello", args={"name": "test"}).abi_result.return_value == "Hello ABI, test"
 
 
 def test_deploy_with_create_args(client_fixture: ApplicationClient, app_spec: ApplicationSpecification) -> None:
     create_args = next(m for m in app_spec.contract.methods if m.name == "create_args")
     client_fixture.deploy("v1", create_args=ABICallArgs(method=create_args, args={"greeting": "deployed"}))
 
-    assert client_fixture.call("hello", args={"name": "test"}).abi_results[0].return_value == "deployed, test"
+    assert client_fixture.call("hello", args={"name": "test"}).abi_result.return_value == "deployed, test"
 
 
 def test_deploy_with_bare_create(client_fixture: ApplicationClient) -> None:
     client_fixture.deploy(
         "v1",
         create_args=ABICallArgs(
-            method=None,
+            method=False,
         ),
     )
 
-    assert client_fixture.call("hello", args={"name": "test"}).abi_results[0].return_value == "Hello Bare, test"
+    assert client_fixture.call("hello", args={"name": "test"}).abi_result.return_value == "Hello Bare, test"
--- tests/test_deploy_scenarios.py
+++ tests/test_deploy_scenarios.py
@@ -10,7 +10,7 @@ from algokit_utils.app import (
 )
 from algokit_utils.application_client import ApplicationClient, OnSchemaBreak, OnUpdate, get_next_version
 from algokit_utils.application_specification import ApplicationSpecification
-from algokit_utils.logic_error import LogicException
+from algokit_utils.logic_error import LogicError
 from algokit_utils.network_clients import get_algod_client, get_indexer_client
 from conftest import check_output_stability, get_specs, get_unique_name, read_spec
 
@@ -37,7 +37,7 @@ class DeployFixture:
         allow_delete: bool | None = None,
         allow_update: bool | None = None,
     ) -> ApplicationClient:
-        app_client = ApplicationClient(self.algod_client, self.indexer_client, app_spec, self.creator)
+        app_client = ApplicationClient(self.algod_client, self.indexer_client, app_spec, creator=self.creator)
         response = app_client.deploy(
             version=version,
             on_update=on_update,
@@ -105,7 +105,7 @@ def test_deploy_app_with_existing_immutable_app_fails(deploy_fixture: DeployFixt
     app_v1 = deploy_fixture.deploy(v1, version="1.0")
     assert app_v1.app_id
 
-    with pytest.raises(LogicException) as error:
+    with pytest.raises(LogicError) as error:
         deploy_fixture.deploy(v2, version="2.0")
     logger.error(f"LogicException: {error.value.message}")
 
@@ -157,7 +157,7 @@ def test_deploy_app_with_existing_permanent_app_on_update_equals_replace_app_fai
     app_v1 = deploy_fixture.deploy(v1, version="1.0")
     assert app_v1.app_id
 
-    with pytest.raises(LogicException) as error:
+    with pytest.raises(LogicError) as error:
         deploy_fixture.deploy(v2, version="3.0", on_update=OnUpdate.ReplaceApp)
     all_apps = deploy_fixture.indexer_client.lookup_account_application_by_creator(deploy_fixture.creator.address)[
         "applications"
@@ -175,11 +175,11 @@ def test_deploy_app_with_existing_permanent_app_and_on_schema_break_equals_repla
 ):
     v1, _, v3 = get_specs(deletable=False)
 
-    app_v1 = deploy_fixture.deploy(v1, "1.0")
+    app_v1 = deploy_fixture.deploy(v1, version="1.0")
     assert app_v1.app_id
 
-    with pytest.raises(LogicException) as exc_info:
-        deploy_fixture.deploy(v3, "3.0", on_schema_break=OnSchemaBreak.ReplaceApp)
+    with pytest.raises(LogicError) as exc_info:
+        deploy_fixture.deploy(v3, version="3.0", on_schema_break=OnSchemaBreak.ReplaceApp)
 
     logger.error(f"Deployment failed: {exc_info.value.message}")
 
@@ -198,7 +198,7 @@ def test_deploy_templated_app_with_changing_parameters_succeeds(deploy_fixture: 
     )
 
     response = app_client.call("hello", args={"name": "call_1"})
-    logger.info(f"Called hello: {response.abi_results[0].return_value}")
+    logger.info(f"Called hello: {response.abi_result.return_value}")
 
     logger.info("Deploy V2 as immutable, deletable")
     app_client = deploy_fixture.deploy(
@@ -208,10 +208,10 @@ def test_deploy_templated_app_with_changing_parameters_succeeds(deploy_fixture: 
     )
 
     response = app_client.call("hello", args={"name": "call_2"})
-    logger.info(f"Called hello: {response.abi_results[0].return_value}")
+    logger.info(f"Called hello: {response.abi_result.return_value}")
 
     logger.info("Attempt to deploy V3 as updatable, deletable, it will fail because V2 was immutable")
-    with pytest.raises(LogicException) as exc_info:
+    with pytest.raises(LogicError) as exc_info:
         # try to make it updatable again
         deploy_fixture.deploy(
             app_spec,
@@ -221,7 +221,7 @@ def test_deploy_templated_app_with_changing_parameters_succeeds(deploy_fixture: 
 
     logger.error(f"LogicException: {exc_info.value.message}")
     response = app_client.call("hello", args={"name": "call_3"})
-    logger.info(f"Called hello: {response.abi_results[0].return_value}")
+    logger.info(f"Called hello: {response.abi_result.return_value}")
 
     logger.info("2nd Attempt to deploy V3 as updatable, deletable, it will succeed as on_update=OnUpdate.DeleteApp")
     # deploy with allow_delete=True, so we can replace it
@@ -233,14 +233,18 @@ def test_deploy_templated_app_with_changing_parameters_succeeds(deploy_fixture: 
         allow_update=True,
     )
     response = app_client.call("hello", args={"name": "call_4"})
-    logger.info(f"Called hello: {response.abi_results[0].return_value}")
+    logger.info(f"Called hello: {response.abi_result.return_value}")
     app_id = app_client.app_id
 
     app_client = ApplicationClient(
-        deploy_fixture.algod_client, deploy_fixture.indexer_client, app_spec, app_id, signer=deploy_fixture.creator
+        deploy_fixture.algod_client,
+        deploy_fixture.indexer_client,
+        app_spec,
+        app_id=app_id,
+        signer=deploy_fixture.creator,
     )
     response = app_client.call("hello", args={"name": "call_5"})
-    logger.info(f"Called hello: {response.abi_results[0].return_value}")
+    logger.info(f"Called hello: {response.abi_result.return_value}")
 
     deploy_fixture.check_log_stability()
 
@@ -269,21 +273,21 @@ def test_deploy_with_schema_breaking_change(
     v3 = read_spec("app_v3.json")
 
     app_v1 = deploy_fixture.deploy(
-        v1, "1.0", allow_delete=deletable == deletable.Yes, allow_update=updatable == updatable.Yes
+        v1, version="1.0", allow_delete=deletable == deletable.Yes, allow_update=updatable == updatable.Yes
     )
     assert app_v1.app_id
 
     try:
         deploy_fixture.deploy(
             v3,
-            "3.0",
+            version="3.0",
             allow_delete=deletable == deletable.Yes,
             allow_update=updatable == updatable.Yes,
             on_schema_break=on_schema_break,
         )
     except DeploymentFailedError as error:
         logger.error(f"DeploymentFailedError: {error}")
-    except LogicException as error:
+    except LogicError as error:
         logger.error(f"LogicException: {error.message}")
 
     deploy_fixture.check_log_stability()
@@ -303,21 +307,21 @@ def test_deploy_with_update(
     v2 = read_spec("app_v2.json")
 
     app_v1 = deploy_fixture.deploy(
-        v1, "1.0", allow_delete=deletable == deletable.Yes, allow_update=updatable == updatable.Yes
+        v1, version="1.0", allow_delete=deletable == deletable.Yes, allow_update=updatable == updatable.Yes
     )
     assert app_v1.app_id
 
     try:
         deploy_fixture.deploy(
             v2,
-            "2.0",
+            version="2.0",
             allow_delete=deletable == deletable.Yes,
             allow_update=updatable == updatable.Yes,
             on_update=on_update,
         )
     except DeploymentFailedError as error:
         logger.error(f"DeploymentFailedError: {error}")
-    except LogicException as error:
+    except LogicError as error:
         logger.error(f"LogicException: {error.message}")
 
     deploy_fixture.check_log_stability()
