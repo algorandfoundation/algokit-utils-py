513dd4c734211fa60d98e5d7b8d01b9aa6b3f830
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2023-03-23 22:48:18 +0800

    fix: get_account now works if called a second time for the same account name

---
+++ tests/test_account.py
@@ -0,0 +1,11 @@
+from algokit_utils import get_account
+from algosdk.v2client.algod import AlgodClient
+from conftest import get_unique_name
+
+
+def test_account_can_be_called_twice(algod_client: AlgodClient) -> None:
+    account_name = get_unique_name()
+    account1 = get_account(algod_client, account_name)
+    account2 = get_account(algod_client, account_name)
+
+    assert account1 == account2
