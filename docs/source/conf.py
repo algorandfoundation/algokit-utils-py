65b87b6669fd463eacc4ca218ee8aef0f2ea7c66
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2023-06-06 14:57:57 +0800

    docs: add handwritten documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ PythonDomain.object_types["data"].roles = ("data", "class", "obj")
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_static_path = []
 
 
 # -- Options for myst ---
