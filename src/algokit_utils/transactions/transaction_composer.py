19e90cb47063986663edb55ed9599cce664814b1
CiottiGiorgio <giorgio.ciotti@algorand.foundation>
2025-05-14 19:57:17 +0200

    feat: using AlgoKit Core to build a payment transaction

--- src/algokit_utils/transactions/transaction_composer.py
+++ src/algokit_utils/transactions/transaction_composer.py
@@ -29,6 +29,7 @@ from algokit_utils.config import config
 from algokit_utils.models.state import BoxIdentifier, BoxReference
 from algokit_utils.models.transaction import SendParams, TransactionWrapper
 from algokit_utils.protocols.account import TransactionSignerAccountProtocol
+from algokit_utils.transactions.algokit_core_bridge import payment_through_core
 
 if TYPE_CHECKING:
     from collections.abc import Callable
@@ -2250,7 +2251,7 @@ class TransactionComposer:
             "close_remainder_to": params.close_remainder_to,
         }
 
-        return self._common_txn_build_step(lambda x: algosdk.transaction.PaymentTxn(**x), params, txn_params)
+        return self._common_txn_build_step(lambda x: payment_through_core(**x), params, txn_params)
 
     def _build_asset_create(
         self, params: AssetCreateParams, suggested_params: algosdk.transaction.SuggestedParams
