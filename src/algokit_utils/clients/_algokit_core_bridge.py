6fa8b559587f1775834750218222b35e7cd22545
CiottiGiorgio <giorgio.ciotti@algorand.foundation>
2025-05-20 18:58:01 +0200

    feat: using AlgodApi OpenAPI generated client to send transactions to a node

---
+++ src/algokit_utils/clients/_algokit_core_bridge.py
@@ -0,0 +1,50 @@
+import base64
+from collections.abc import Iterable
+
+import algokit_algod_api
+from algosdk.encoding import msgpack_encode
+from algosdk.transaction import GenericSignedTransaction
+from algosdk.v2client.algod import AlgodClient
+
+
+class AlgodClientWithCore:
+    """
+    A decorator for AlgodClient that extends its functionality with algokit_algod_api capabilities.
+    This class wraps an AlgodClient instance while maintaining the same interface.
+    """
+
+    def __init__(self, algod_client: AlgodClient):
+        """
+        Initialize the AlgodClientWithCore with an existing AlgodClient.
+
+        Args:
+            algod_client: The AlgodClient instance to wrap
+        """
+        self._algod_client = algod_client
+
+        configuration = algokit_algod_api.Configuration(
+            host=algod_client.algod_address,
+        )
+        api_client = algokit_algod_api.ApiClient(configuration)
+        api_client.default_headers.update({"X-Algo-API-Token": self._algod_client.algod_token})
+        self._algod_core_client = algokit_algod_api.AlgodApi(api_client=api_client)
+
+    def send_raw_transaction(self, txn):
+        """
+        Override the method to send a raw transaction using algokit_algod_api.
+        """
+        return self._algod_core_client.raw_transaction(base64.b64decode(txn))
+
+    def send_transactions(self, txns: Iterable[GenericSignedTransaction]):
+        """
+        Override the method to send multiple transactions using algokit_algod_api.
+        """
+        return self.send_raw_transaction(
+            base64.b64encode(b"".join(base64.b64decode(msgpack_encode(txn)) for txn in txns))
+        )
+
+    def __getattr__(self, name):
+        """
+        Delegate all other method calls to the wrapped AlgodClient instance.
+        """
+        return getattr(self._algod_client, name)
