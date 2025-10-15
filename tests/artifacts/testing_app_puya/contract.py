89cfe151c16bd9fb53ea5e09da84db0f4ac1f2a6
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-10-15 15:34:01 +0800

    test: add test for resource population of a box and app

--- tests/artifacts/testing_app_puya/contract.py
+++ tests/artifacts/testing_app_puya/contract.py
@@ -1,11 +1,20 @@
 from typing import Literal
 
-from algopy import ARC4Contract, BoxMap, Bytes, arc4, op
+from algopy import Application, ARC4Contract, Box, BoxMap, Bytes, arc4, op
 
 
 class DummyStruct(arc4.Struct):
     name: arc4.String
     id: arc4.UInt64
+
+
+class External(ARC4Contract):
+    def __init__(self) -> None:
+        self.box = Box(Bytes)
+
+    @arc4.abimethod
+    def set_box(self) -> None:
+        self.box.value = Bytes(b"foo")
 
 
 class TestPuyaBoxes(ARC4Contract):
@@ -16,6 +25,7 @@ class TestPuyaBoxes(ARC4Contract):
         self.box_int = BoxMap(arc4.String, arc4.UInt32)
         self.box_int512 = BoxMap(arc4.String, arc4.UInt512)
         self.box_static = BoxMap(arc4.String, arc4.StaticArray[arc4.Byte, Literal[4]])
+        self.external = Application(0)
 
     @arc4.abimethod
     def set_box_bytes(self, name: arc4.String, value: Bytes) -> None:
@@ -41,3 +51,15 @@ class TestPuyaBoxes(ARC4Contract):
     def set_struct(self, name: arc4.String, value: DummyStruct) -> None:
         assert name.bytes == value.name.bytes, "Name must match id of struct"
         op.Box.put(name.bytes, value.bytes)
+
+    @arc4.abimethod
+    def bootstrap_external_app(self) -> Application:
+        self.external = arc4.arc4_create(External).created_app
+        return self.external
+
+    @arc4.abimethod
+    def set_external_box(self) -> None:
+        arc4.abi_call(
+            External.set_box,
+            app_id=self.external,
+        )
