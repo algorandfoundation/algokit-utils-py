351307e4122b6999b9041266ca051de6b536a7a8
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-11-03 15:47:28 +0800

    test: remove module test for modules with implicit coverage

--- tests/modules/test_modules_exist.py
+++ tests/modules/test_modules_exist.py
@@ -6,8 +6,6 @@ import pytest
 @pytest.mark.parametrize(
     "module_name",
     [
-        "algokit_transact",
-        "algokit_common",
         "algokit_abi",
         "algokit_algod_client",
         "algokit_kmd_client",
