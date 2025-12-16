1592342d1a7b71928b7c09d21db93a231d295b63
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2025-12-16 18:42:17 +0100

    fix(ci): remove redundant gh release edit from prerelease steps

--- src/algokit_utils/clients/__init__.py
+++ src/algokit_utils/clients/__init__.py
@@ -1,2 +1,41 @@
-from algokit_utils.clients.client_manager import *  # noqa: F403
-from algokit_utils.clients.dispenser_api_client import *  # noqa: F403
+from algokit_algod_client import AlgodClient
+from algokit_algod_client import models as algod_models
+from algokit_algod_client.exceptions import UnexpectedStatusError
+from algokit_indexer_client import IndexerClient
+from algokit_kmd_client import KmdClient
+from algokit_utils.clients.client_manager import (
+    AlgoSdkClients,
+    ClientManager,
+    NetworkDetail,
+)
+from algokit_utils.clients.dispenser_api_client import (
+    DISPENSER_ACCESS_TOKEN_KEY,
+    DISPENSER_ASSETS,
+    DISPENSER_REQUEST_TIMEOUT,
+    DispenserApiConfig,
+    DispenserAsset,
+    DispenserAssetName,
+    DispenserFundResponse,
+    DispenserLimitResponse,
+    TestNetDispenserApiClient,
+)
+
+__all__ = [
+    "DISPENSER_ACCESS_TOKEN_KEY",
+    "DISPENSER_ASSETS",
+    "DISPENSER_REQUEST_TIMEOUT",
+    "AlgoSdkClients",
+    "AlgodClient",
+    "ClientManager",
+    "DispenserApiConfig",
+    "DispenserAsset",
+    "DispenserAssetName",
+    "DispenserFundResponse",
+    "DispenserLimitResponse",
+    "IndexerClient",
+    "KmdClient",
+    "NetworkDetail",
+    "TestNetDispenserApiClient",
+    "UnexpectedStatusError",
+    "algod_models",
+]
