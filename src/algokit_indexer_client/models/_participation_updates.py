17703090591782706c0cce08f52947584e3b4f3f
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-12-22 12:12:16 +0800

    chore: refresh clients

--- src/algokit_indexer_client/models/_participation_updates.py
+++ src/algokit_indexer_client/models/_participation_updates.py
@@ -12,11 +12,11 @@ class ParticipationUpdates:
     Participation account data that needs to be checked/acted on by the network.
     """
 
-    absent_participation_accounts: list[str] | None = field(
-        default=None,
+    absent_participation_accounts: list[str] = field(
+        default_factory=list,
         metadata=wire("absent-participation-accounts"),
     )
-    expired_participation_accounts: list[str] | None = field(
-        default=None,
+    expired_participation_accounts: list[str] = field(
+        default_factory=list,
         metadata=wire("expired-participation-accounts"),
     )
